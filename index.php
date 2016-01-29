8cfa650481fdd9072bdfa234b98d58f6ca803cff
Jonny Porter <myctrring@gmail.com>
2016-01-28 17:56:31 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Home - Jonny Porter"
+?>
+<?php include "header2.php" ?>
+
+    <div class="pagecontent">
+      <h1 style="text-align: center;"><u>Web Developer</u></h1>
+      <p style="text-align: center;">Welcome. I am Jonny Porter, and this is the home page to my website portfolio.
+      </p>
+    </div>
+
+    <center> <img src="profile.png" style="width: 40%; box-shadow: 0 0 25px black; border-radius: 1%;" href="#"> </center>
+
+    <p style="text-align:center;">Think those bubbles down there are cool? You can make it too at <a href="https://www.codecademy.com/" class="link-on-white" target="_blank">Codecademy</a>.</p>
+
+<div id="title_message">
+      <center>  <canvas id="myCanvas"></canvas>
+    <script type="text/javascript" src="http://s3.amazonaws.com/codecademy-content/courses/hour-of-code/js/bubbles.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    </center>
+    </div>
+
+<?php include "footer2.php" ?>
---
+++ resume.php
@@ -0,0 +1,36 @@
+
+<?php $title = "Extra"; ?>
+
+<?php include "header2.php" ?>
+
+<center><h1>Nothing on the bulletin today!</h1></center>
+
+<!--
+    <div class="container text-center">
+
+    <h1>Angular Developer</h1>
+
+    <h2>Summary</h2>
+
+    <p>For me, technology has always been a fascinating and amazing field. It never ceases to amaze.
+    I am a tech enthusiast who loves to make things from code, especially when I know what I'm doing!
+    I also enjoy learning new programming languages and figuring out problems.
+    I am always looking to gain experience in JavaScript, and I understand HTML and CSS.</p>
+
+        <img src="profile.JPG" style="max-width:50%" align="left" hspace="20" vspace="30">
+
+       <div class="panel-heading">
+        <h2 class="panel-title">Skills<br/>or<br/>things im aiming for</h4>
+        <div class="panel-body">
+        <h4>Languages</h4>
+        <span>HTML, CSS, JavaScript</span>
+         <h4>Frameworks</h4>
+         <span>Angular, Ionic</span>
+         <h4>Tools</h4>
+         <span>Git, SourceTree, VS Code</span>
+         <h4>Other things about me</h4>
+         <span>Love getting up to date on tech news, learning Japanese, playing the ukulele, or spoiling my <a href="http://jonnyporter.x10host.com/gavin/index.html" target="_blank" class="link-on-white">nephews</a>.</span>
+        </div>
+</div>
+-->
+<?php include "footer2.php" ?>
